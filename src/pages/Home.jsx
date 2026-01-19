4d7ca6cc38a43a8928d564e1cd2ed1eecc96b3ed
Parash Agrawal <parashagrawal75@gmail.com>
2026-01-19 07:47:01 +0545

    Handle on clear search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,6 +25,8 @@ export default function Home() {
             } finally {
                 setLoading(false);
             }
+        } else {
+            fetchAndSetPopularMovies();
         }
     }
 
